90f9b15b84ce4cb0b60283bdc21cbe936f95b415
Gouri-Biju <gouribiju24@gmail.com>
2025-09-07 13:47:24 +0530

    Debugging2

--- CareerGuidanceandCollegeAdmissionChancesPredictionSystem/settings.py
+++ CareerGuidanceandCollegeAdmissionChancesPredictionSystem/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'rest_framework',
-    'CareerGuidanceandCollegeAdmissionChancesPredictionSystem.guidanceapp',
+    'guidanceapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
